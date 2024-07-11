9dff8594da97c18affdd285ed1e4fe061d087e27
BEB 09 박상현 <psh03225@naver.com>
2024-07-12 01:15:48 +0900

    UPADTE: getPublicKeyFromTxHash

--- ethereum_getPubkey.js
+++ ethereum_getPubkey.js
@@ -1,11 +1,8 @@
 const { ethers } = require('ethers');
 
-// Function to extract the address associated with to a transaction
 async function getPublicKeyFromTransactionHash(provider, txHash) {
-  // Fetch the transaction using the transaction hash and provier
   const tx = await provider.getTransaction(txHash);
 
-  // Extract the all the relevant fields from the transaction (We need all of them)
   const unsignedTx = {
     gasLimit: tx.gasLimit,
     value: tx.value,
@@ -18,35 +15,34 @@ async function getPublicKeyFromTransactionHash(provider, txHash) {
     maxPriorityFeePerGas: tx.maxPriorityFeePerGas,
   };
 
-  // Serializing tx without the signature
   const serializedTx = ethers.utils.serializeTransaction(unsignedTx);
 
-  // Extract the signature (v, r, s) from the transaction
   const { v, r, s } = tx;
 
-  // Join splitted signature
   const signature = ethers.utils.joinSignature({ v, r, s });
 
-  // Recover the address or public key with (replace recoverAddress by recoverPublicKey) associated with the transaction
-  return ethers.utils.recoverAddress(
+  return ethers.utils.recoverPublicKey(
     ethers.utils.keccak256(serializedTx),
     signature
   );
 }
 
-// Call the function with a provider and a transaction hash
+// calculate account address from public key
+async function getAccountAddressFromPublicKey(publicKey) {
+  const address = ethers.utils.computeAddress(publicKey);
+  return address;
+}
+
 (async () => {
-  // Public provider for Goerli
   const provider = new ethers.providers.JsonRpcProvider(
-    'https://mainnet.infura.io/v3/64399f7381854c4ebb02ae69db93c72c'
+    'https://mainnet.infura.io/v3/MY_INFURA_PROJECT_ID'
   );
 
-  // Transaction hash
   const txHash =
-    '0xda86cb8fbfa5ec2cc3c1730d785b548df0de827ffb139c258a954128b1c463cd';
+    '0x9a785725ad9a6ce23927d7965f946514c269914187201d3a3c51f22e9981a9a6';
 
-  // And finally call the function to extract the address associated with the transaction
   const address = await getPublicKeyFromTransactionHash(provider, txHash);
 
-  console.log('Address:', address);
+  console.log('publick key:', address);
+  console.log('address:', await getAccountAddressFromPublicKey(address));
 })();
